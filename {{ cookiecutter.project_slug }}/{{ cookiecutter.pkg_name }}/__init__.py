9ff992476bee1b6d1af2e9b04ef343f6f0d91134
Timothy McFadden <mtik00@users.noreply.github.com>
2023-08-21 11:38:29 -0600

    feat: adding __main__ to sample code

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print("Hello, World!")
-print("...from {{ cookiecutter.project_slug }}")
+
+def main():
+    print("Hello, World!")
+    print("...from {{ cookiecutter.pkg_name }}")
+
+
+if __name__ == "__main__":
+    main()
